fc66b600d442888eddec8b77ef709bb0e61e17ee
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-11-25 07:10:50 +0100

    Change app structure to package

--- application/app.py
+++ application/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -8,9 +8,10 @@ app = Flask(__name__)
 def index():
     return "Welcome to the index page"
 
-@app.route("/hello")
-def hello_world():
-    return "Hello World"
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_world(name=None):
+    return render_template("hello.html", name=name)
 
 @app.route("/user/<username>")
 def show_user_profile(username):
@@ -32,3 +33,12 @@ with app.test_request_context():
     print(url_for("show_path", subpath="/seun/"))
     print(url_for("show_path", subpath=""))
     print(url_for("show_path", subpath="Miles Ahead"))
+    print(url_for("static", filename="style.css"))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return do_the_login()
+    else:
+        return show_the_login_form() 
+    
